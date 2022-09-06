e02fb210da3040d481c751977120ffe6a86dafb0
princelee <noli.b.bautista@outlook.com>
2022-09-06 16:03:29 +0800

    updated weather.py

--- weather.py
+++ weather.py
@@ -1,14 +1,60 @@
+# importing modules
+import datetime as dt
 import requests
+import json
 
-url = "https://weatherbit-v1-mashape.p.rapidapi.com/current"
+# API base URL
+BASE_URL = "https://api.openweathermap.org/data/2.5/weather?"
 
-querystring = {"lang": "en", "lon": "<required>", "lat": "<required>"}
+# City Name
+CITY = input("Please Enter City:\n")
 
-headers = {
-    'x-rapidapi-host': "weatherbit-v1-mashape.p.rapidapi.com",
-    'x-rapidapi-key': "96e378cea5msh261b20bbf9c9ba8p1d5686jsndec440cbf6f2"
-    }
+# Your API key
+API_KEY = "c2e250c0477f3a47bdc37092969e33b9"
 
-response = requests.request("GET", url, headers=headers, params=querystring)
+# updating the URL
+URL = BASE_URL + "appid=" + API_KEY + "&q=" + CITY
 
-print(response.text)
+# Sending HTTP request
+response = requests.get(URL)
+
+
+# checking the status code of the request
+if response.status_code == 200:
+
+    # retrieving data in the json format
+    data = response.json()
+
+    # kelvin to Celsius variable
+    KtoC = -272.15
+
+    # take the main dict block
+    main = data['main']
+
+    # getting temperature
+    temperature = main['temp']
+    temperature = temperature * KtoC
+
+    # getting feel like
+    temp_feel_like = main['feels_like']
+    temp_feel_like = temp_feel_like * KtoC
+
+    # getting the humidity
+    humidity = main['humidity']
+
+    # weather report
+    weather_report = data['weather']
+
+    # wind report
+    wind_report = data['wind']
+
+    print(f"{CITY:-^35}")
+    print(f"Temperature: {temperature} degree celsius")
+    print(f"Feel Like: {temp_feel_like} degree celcius")
+    print(f"Humidity: {humidity}")
+    print(f"Weather Report: {weather_report[0]['description']}")
+    print(f"Wind Speed: {wind_report['speed']}")
+else:
+    # showing the error message
+    print("Error in the HTTP request")
+
